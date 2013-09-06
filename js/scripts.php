60de94985497cb4227ccfe145318c9425cb4adef
William Mincy <william.mincy@jwt.com>
2013-09-06 10:34:39 -0400

    + Added handler for unset value throwing error

--- js/scripts.php
+++ js/scripts.php
@@ -9,7 +9,8 @@
 <script>
 	/* <![CDATA[ */
 	jQuery(document).ready(function(e) {
-		$cp_copied = JSON.parse(jQuery.cookie('copyPasta_copied'));
+		$cp_copied = [];
+		try { JSON.parse(jQuery.cookie('copyPasta_copied')); } catch(e) { }
 		// new instance of the container 
 		jQuery('div.toolbox-handle')
 			.first()
